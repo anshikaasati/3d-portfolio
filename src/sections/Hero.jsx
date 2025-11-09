312ba1b1311443e95da30348fe03b415855131c9
Anshika Asati <134847046+anshikaasati@users.noreply.github.com>
2025-11-09 14:42:05 +0530

    position updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Anshika!<span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Software Developer Engineer</p>
+        <p className="hero_tag text-gray_gradient">Backend Developer</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
